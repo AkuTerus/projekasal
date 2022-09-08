c9fb208c8065ef6ce0b590721cff7e65a18e1abf
Bambang <bambang.s4p@gmail.com>
2022-09-08 19:35:46 +0700

    ubah collapsible untuk elemen .social (& ubah icon toggler)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const toggler = document.querySelector('.toggler');
 
-const targetClass = '.collapsible';
+const targetClass = '.social';
 const toggleTarget = (displayState) => {
   document.querySelectorAll(targetClass).forEach((target) => {
     target.style.display = displayState;
@@ -11,10 +11,12 @@ let n = 0;
 toggler.addEventListener('click', function (e) {
   if (n % 2 == 0) {
     toggleTarget('none');
-    toggler.innerText = 'vv';
+    toggler.innerText = '^^';
+    toggler.style.marginTop = '-20px'; // margin top
   } else {
     toggleTarget('');
-    toggler.innerText = '^^';
+    toggler.innerText = 'X';
+    toggler.style.marginTop = '-44px'; // margin top
   }
   n++;
 });
